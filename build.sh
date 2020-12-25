ebcccb98977c05973d718b37da065db953768bf3
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-12-25 13:17:36 -0600

    initial

--- build.sh
+++ build.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-
-if [ "$OS" = "Windows_NT" ]; then
-    ./mingw64.sh
-    exit 0
-fi
+set -exo pipefail
+[[ -n "$1" ]]
 
 make clean || echo clean
 
-rm -f config.status
+rm -f config.status Makefile.in
 ./autogen.sh
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    ./nomacro.pl
-    ./configure \
-        CFLAGS="-march=native -O2 -Ofast -flto -DUSE_ASM -pg" \
-        --with-crypto=/usr/local/opt/openssl \
-        --with-curl=/usr/local/opt/curl
-    make -j4
-    strip cpuminer
-    exit 0
-fi
+if [[ "$1" = PGO-1 ]] ; then
+
+rm -rf /tmp/cpuminer-multi.gcda /tmp/cpuminer-multi *.gcno
 
 # Linux build
 
@@ -28,14 +18,26 @@ fi
 
 # Debian 7.7 / Ubuntu 14.04 (gcc 4.7+)
 extracflags="$extracflags -Ofast -flto -fuse-linker-plugin -ftree-loop-if-convert-stores"
+extracflags="$extracflags -D_REENTRANT -funroll-loops -fvariable-expansion-in-unroller -fmerge-all-constants -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
+extracflags="$extracflags -march=native -mtune=native -g0 -Ofast -ffast-math -fassociative-math -freciprocal-math -fmerge-all-constants -fipa-pta -floop-nest-optimize -fgraphite-identity -floop-parallelize-all"
+PGFLAGS="-fprofile-generate=/tmp/cpuminer-multi.gcda -fipa-profile -fprofile-reorder-functions -fvpt -fprofile -fprofile-abs-path -fprofile-arcs -fprofile-dir=/tmp/cpuminer-multi -fprofile-reproduciblemultithreaded"
+# CFLAGS -pg
+# LDFLAGS -pg
+# -static
 
-if [ ! "0" = `cat /proc/cpuinfo | grep -c avx` ]; then
-    # march native doesn't always works, ex. some Pentium Gxxx (no avx)
-    extracflags="$extracflags -march=native"
-fi
+#./configure --with-crypto --with-curl CFLAGS="$extracflags -DUSE_ASM -pg"
+./configure --with-crypto --with-curl PGFLAGS="$PGFLAGS" CFLAGS="$extracflags -DUSE_ASM" LDFLAGS='-lgcov --coverage'
 
-./configure --with-crypto --with-curl CFLAGS="-O2 $extracflags -DUSE_ASM -pg"
+elif [[ "$1" = PGO-2 ]] ; then
 
-make -j 4
+extracflags="$extracflags -Ofast -flto -fuse-linker-plugin -ftree-loop-if-convert-stores"
+extracflags="$extracflags -D_REENTRANT -funroll-loops -fvariable-expansion-in-unroller -fmerge-all-constants -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
+extracflags="$extracflags -march=native -mtune=native -g0 -Ofast -ffast-math -fassociative-math -freciprocal-math -fmerge-all-constants -fipa-pta -floop-nest-optimize -fgraphite-identity -floop-parallelize-all"
+PGFLAGS="-fprofile-use -fprofile-dir=/tmp/cpuminer-multi -fprofile-correction"
+./configure --with-crypto --with-curl PGFLAGS="$PGFLAGS" CFLAGS="$extracflags -DUSE_ASM" # LDFLAGS='-lgcov --coverage'
 
-strip -s cpuminer
+else exit 1 ; fi
+
+make -j$(nproc)
+strip --strip-all cpuminer
+
