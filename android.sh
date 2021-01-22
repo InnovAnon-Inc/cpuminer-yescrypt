40bcf95a051de9df8dc1c83a22e780b49f2830f7
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2021-01-22 16:56:27 -0600

    android

--- android.sh
+++ android.sh
@@ -5,9 +5,9 @@ git clean -fdx
 git clean -fdx
 git pull
 cp -v cpu-miner.c.local cpu-miner.c
-export NWITH_GETLINE=1
+sed -i 's@^#ifdef NWITH_GETLINE@#if true@' sysinfos.c
 ./autogen.sh
-NWITH_GETLINE=1 ./configure CPPFLAGS="$CPPFLAGS -DNWITH_GETLINE=1"
+NWITH_GETLINE=1 ./configure
 make
 make install
 
