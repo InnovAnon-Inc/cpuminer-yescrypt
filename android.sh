ffbee92d016d0a91a3a2d9c1d10b70883da8ffc6
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2021-01-29 20:49:29 -0600

    update

--- android.sh
+++ android.sh
@@ -3,7 +3,8 @@ set -evx
 
 CPPFLAGS="$CPPFLAGS -DNDEBUG"
 #CFLAGS0="-march=native -mtune=native -fipa-profile -fprofile-reorder-functions -fvpt -fprofile-arcs -pg -fprofile-abs-path -fprofile-dir=$HOME/pg -Ofast -g0 -fuse-linker-plugin -flto -ffunction-sections -fdata-sections -ffast-math -fassociative-math -freciprocal-math -fmerge-all-constants -fipa-pta -floop-nest-optimize -fgraphite-identity -floop-parallelize-all"
-CFLAGS0="-Ofast -g0 -fmerge-all-constants"
+CFLAGS0="-fipa-profile -fprofile-reorder-functions -fvpt -fprofile-arcs -pg -fprofile-abs-path -fprofile-dir=$HOME/pg -Ofast -g0 -fuse-linker-plugin -flto -ffunction-sections -fdata-sections -ffast-math -fassociative-math -freciprocal-math -fmerge-all-constants -fipa-pta -floop-nest-optimize -fgraphite-identity -floop-parallelize-all"
+#CFLAGS0="-Ofast -g0 -fmerge-all-constants"
 CFLAGS="$CFLAGS $CFLAGS0"
 CXXFLAGS="$CXXFLAGS $CFLAGS0"
 #LDFLAGS="$LDFLAGS -fipa-profile -fprofile-reorder-functions -fvpt -fprofile-arcs -pg -fprofile-abs-path -fprofile-dir=$HOME/pg -fuse-linker-plugin -flto -Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections"
